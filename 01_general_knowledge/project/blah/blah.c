e12acbce8a95ffb6a61366babf72d461f6f297e6
tam_ubuntu <tamnm20>
2024-07-24 03:04:22 -0700

    240724

---
+++ 01_general_knowledge/project/blah/blah.c
@@ -0,0 +1,9 @@
+// blah.c
+#include <stdio.h>
+int main() 
+{ 	
+	int a = 110;
+	printf("a = %d\n",a);
+	return 0; 
+}
+
--- 01_general_knowledge/project/hello/main.c
+++ 01_general_knowledge/project/hello/main.c
@@ -1,9 +1,24 @@
 #include<stdio.h>
 #include "add.h"
 #include "sub.h"
+
+void test();
+inline void test() {
+    printf("test\n");
+}
+
+void test1() {
+    printf("test1\n");
+
+}
+
 int main(void)
 {
-    printf("Hello world! = %d\n",add(5,6));
+    printf("Hello world! = %d\n",add(6,6));
     printf("Hello world! = %d\n",sub(5,6));
+
+    test();
+    test1();
+
     return 0;
 }
